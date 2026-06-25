1bfd5af01ba23bb8b52f2eecbe83578c951d7940
Jay Wisdom ⚙️ <jsonwisdom@yahoo.com>
2026-06-24 21:42:22 -0500

    test: lock first eas attestation details

--- signal-core/tests/test_eas_manifest.py
+++ signal-core/tests/test_eas_manifest.py
@@ -29,3 +29,13 @@ def test_eas_registration_details_locked():
     assert manifest['schema_uid'] == '0xa5b0d2dd5470542a119d50eba19898f50e1f77591f01d4fec4c6f3075054aa11'
     assert manifest['creator'] == '0xC345B26094c63C69222Ee775189a3d3eaead5a84'
     assert manifest['transaction_hash'] == '0x60f9ac0a4d23b112743cc902f4d3f8f06f239664f9b9cc0c3c2909ef682ea7bf'
+
+
+def test_first_attestation_details_locked():
+    attestation = load_manifest()['first_attestation']
+    assert attestation['attestation_uid'] == '0x84b58fb78cfde7f791b311c07e5982eeffc3f60b550d594dc9407419ed5d5150'
+    assert attestation['transaction_hash'] == '0x539c087bb2987bfa3eae4099b0a37765d1ec824e7c6174169745fc440004cab5'
+    assert attestation['receiptId'] == 'vr:sha256:fb83118511079eca8bb21adc5b24a7c26971f055c061ddcd4a7d7f532b2b759c'
+    assert attestation['receiptCoreHash'] == 'sha256:b2bb199d65e2d4f66cf093fd0d4218341046d892ce3c17cc881c2bda4d66868e'
+    assert attestation['authorityNone'] is True
+    assert attestation['attestation_version'] == 'v0.4'
